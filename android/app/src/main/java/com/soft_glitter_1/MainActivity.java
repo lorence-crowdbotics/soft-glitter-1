b3743d11f1b10c0bfb15a779ce56b5705bf68c92
Lorence Lim <lorence@crowdbotics.com>
2021-01-05 06:42:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soft_glitter_1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soft_glitter_1;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "soft_glitter_1";
+    }
+}
